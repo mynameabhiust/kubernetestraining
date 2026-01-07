dd12c0a02ee15b593ae14a33591898a222e7012c
abhijith <abhijith.muraleedharannair@ust.com>
2026-01-07 23:25:33 +0530

    added code installing ubuntu

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,33 +1,34 @@
-#Write-Host "Installing jdx.mise"
-#
-#winget install jdx.mise --silent --accept-source-agreements --accept-package-agreements
-#
-#Write-Host "Installed jdx.mise"
-#
-#Write-Host "Installing helm"
-#
-#winget install Helm.Helm --silent --accept-source-agreements --accept-package-agreements
-#
-#Write-Host "Installed helm"
+Write-Host "Installing jdx.mise"
 
-#Write-Host "Installing tilt"
-#
-#iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.ps1'))
-#
-#Write-Host "Installed tilt"
+winget install jdx.mise --silent --accept-source-agreements --accept-package-agreements
 
-#Write-Host "Installing dive"
-#
-#winget install --id wagoodman.dive
-#
-#Write-Host "Installing dive"
+Write-Host "Installed jdx.mise"
 
-#Write-Host "Adding to path variables"
-#$env:Path+=";C:\Users\Administrator\bin\tilt.exe"
-#$env:Path+=";C:\Users\Administrator\AppData\Local\Microsoft\WinGet\Links\"
-#$env:Path+=";C:\Program Files\Docker\Docker\resources\bin\kubectl.exe"
-#$env:Path+=";C:\Users\Administrator\AppData\Local\Microsoft\WinGet\Packages\jdx.mise_Microsoft.Winget.Source_8wekyb3d8bbwe\mise\bin"
-#[System.Environment]::SetEnvironmentVariable("Path",$env:Path,"User")
-#
-#Write-Host "Added to path variables"
+Write-Host "Installing helm"
+
+winget install Helm.Helm --silent --accept-source-agreements --accept-package-agreements
+
+Write-Host "Installed helm"
+Write-Host "Installing tilt"
+
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.ps1'))
+
+Write-Host "Installed tilt"
+Write-Host "Installing dive"
+
+winget install --id wagoodman.dive
+
+Write-Host "Installing dive"
+Write-Host "Adding to path variables"
+$env:Path+=";C:\Users\Administrator\bin\tilt.exe"
+$env:Path+=";C:\Users\Administrator\AppData\Local\Microsoft\WinGet\Links\"
+$env:Path+=";C:\Program Files\Docker\Docker\resources\bin\kubectl.exe"
+$env:Path+=";C:\Users\Administrator\AppData\Local\Microsoft\WinGet\Packages\jdx.mise_Microsoft.Winget.Source_8wekyb3d8bbwe\mise\bin"
+[System.Environment]::SetEnvironmentVariable("Path",$env:Path,"User")
+
+Write-Host "Added to path variables"
+
+Write-Host "Installing Ubuntu"
+wsl --install -d Ubuntu-24.04
+Write-Host "Completed installing Ubuntu WSL"
 
