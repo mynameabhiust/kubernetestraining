64148d2c2012ff8f2574c5ed05e0c239512b5a24
Abhijith Muraleedharan Nair(UST <u91814@ust.com>
2026-02-02 09:20:49 +0530

    added codes for pocs

---
+++ api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import io
+
+app = FastAPI()
+
+@app.get("/download")
+def download_dynamic_file():
+    content = "Hello from API!\nThis file was generated dynamically.Yes.12"
+    file_like = io.BytesIO(content.encode())
+
+    return StreamingResponse(
+        file_like,
+        media_type="text/plain",
+        headers={"Content-Disposition": "attachment; filename=generated.txt"}
+    )
--- multistagedemo/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello from multi-stage build 👋");
