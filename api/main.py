1a2041c65769a8c28881fe8870dc26379ddc534c
abhijith <abhijith.muraleedharannair@ust.com>
2026-02-19 17:09:53 +0530

    Added code for demo

--- api/main.py
+++ api/main.py
@@ -1,3 +1,6 @@
+#uvicorn main:app --reload
+#pip install fastapi uvicorn 
+# pip install fastapi uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 import io
@@ -6,7 +9,7 @@ app = FastAPI()
 
 @app.get("/download")
 def download_dynamic_file():
-    content = "Hello from API!\nThis file was generated dynamically.Yes.12"
+    content = "Hello from API .Modified!\nThis file was generated dynamically.Yes.12"
     file_like = io.BytesIO(content.encode())
 
     return StreamingResponse(
